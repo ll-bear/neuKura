42349b896924d355e33f6418a244a92a2a7138c0
Shigeru-Honda <ll.bear.work@gmail.com>
2026-06-04 20:04:22 +0900

    add_config

--- config/webPlot.php
+++ config/webPlot.php
@@ -6,6 +6,7 @@ return [
     'default_user_password' => env('DEFAULT_USER_PASSWORD', 'password'),
     'llm_api_url' => env('LLM_API_URL'),
     'llm_model' => env('LLM_MODEL'),
+    'llm_embed_model' => env('LLM_EMBED_MODEL', 'embeddinggemma'),
     'llm_chat_timeout' => env('LLM_CHAT_TIMEOUT', 120),
     'llm_embed_timeout' => env('LLM_EMBED_TIMEOUT', 180),
 ];
